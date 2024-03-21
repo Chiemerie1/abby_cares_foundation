d06e9bfdaba9ad02d0a77d8390f9d0c23550715f
Chiemerie Okoro <chiemeriedroid@gmail.com>
2024-03-21 17:33:56 +0100

    initial commit

---
+++ main/urls.py
@@ -0,0 +1,22 @@
+
+from django.contrib import admin
+from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
+from .import views
+
+
+
+app_name="main"
+
+
+
+
+urlpatterns = [
+    path("", views.home, name="home"),
+]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
