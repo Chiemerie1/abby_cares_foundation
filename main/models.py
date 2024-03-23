07a4a8370f4ab4af0862e7076bdbbd565a815f7e
Chiemerie Okoro <chiemeriedroid@gmail.com>
2024-03-23 01:01:44 +0100

    current progress

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class NewsAndUpdate(models.Model):
+    image = models.ImageField(
+        blank=False,
+        upload_to="homepage_what_we_have_done"
+    )
+    Headline = models.CharField(max_length=30)
+    paragraph = models.TextField()
+
+    def snip(self):
+        return self.paragraph[:20] + "..."
+    
+    def __str__(self):
+        return self.Headline
+    
